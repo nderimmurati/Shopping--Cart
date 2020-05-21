77d56ebe45aaa89960c66adbebef77b152fd0e9a
qendriimr <qr20339@ubt-uni.net>
2020-05-21 14:21:24 +0200

    Krijimi login formes, logout, payment, produktet

---
+++ newslettter.php
@@ -0,0 +1,39 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+		
+			<div class="container">
+
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Regjistrohu per <strong>Ofertat e Reja</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                              
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="#"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+								</li>
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+			
+			</div>
+			
+		</div>
